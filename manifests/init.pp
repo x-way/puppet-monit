984b05dd517a1f6bf2514b557375177d437f4c9b
root <root@puppet.jaggi.lan>
2013-04-19 15:13:18 +0200

    Adapted for puppet 3.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,93 @@
-import "*"
+class monit {
+	package { 'monit':
+		ensure => installed,
+	}
+	
+	service { 'monit':
+		ensure => running,
+		enable => true,
+		hasrestart => true,
+		require => Package['monit'],
+	}
 
+	file { '/etc/monit/monitrc':
+		mode => 600,
+		owner => root,
+		group => root,
+		content => template("/etc/puppet/modules/monit/templates/monitrc.erb"),
+		notify => Service['monit'],
+		require => File['/etc/monit'],
+	}
+
+	file { '/etc/default/monit':
+		mode => 644,
+		owner => root,
+		group => root,
+		content => template("/etc/puppet/modules/monit/templates/default.erb"),
+		notify => Service['monit'],
+		require => Package['monit'],
+	}
+
+	file { '/etc/monit/':
+		mode => 755,
+		owner => root,
+		group => root,
+		ensure => directory,
+		notify => Service['monit'],
+		require => Package['monit'],
+	}
+	file { '/etc/monit/conf.d':
+		mode => 755,
+		owner => root,
+		group => root,
+		ensure => directory,
+		require => File['/etc/monit'],
+		notify => Service['monit'],
+		recurse => true,
+		purge => true,
+	}
+}
+
+define monit::options ($interval, $mailserver, $email, $http_address, $http_allow, $http_user, $http_password) {
+	file { "/etc/monit/conf.d/options_$name":
+		mode => 600,
+		owner => root,
+		group => root,
+		content => template("/etc/puppet/modules/monit/templates/options.erb"),
+		notify => Service['monit'],
+		require => File['/etc/monit/conf.d'],
+	}
+}
+
+define monit::pidcheck ($process_name, $pidfile, $start_prog, $stop_prog) {
+	file { "/etc/monit/conf.d/$name":
+		mode => 600,
+		owner => root,
+		group => root,
+		content => template("/etc/puppet/modules/monit/templates/pidcheck.erb"),
+		notify => Service['monit'],
+		require => File['/etc/monit/conf.d'],
+	}
+}
+
+define monit::bindcheck ($bind_ips) {
+	file { "/etc/monit/conf.d/$name":
+		mode => 600,
+		owner => root,
+		group => root,
+		content => template("/etc/puppet/modules/monit/templates/bind.erb"),
+		notify => Service['monit'],
+		require => File['/etc/monit/conf.d'],
+	}
+}
+
+define monit::clamdcheck ($clamd_port=3310, $clamd_ips=[]) {
+	file { "/etc/monit/conf.d/$name":
+		mode => 600,
+		owner => root,
+		group => root,
+		content => template("/etc/puppet/modules/monit/templates/clamd.erb"),
+		notify => Service['monit'],
+		require => File['/etc/monit/conf.d'],
+	}
+}
